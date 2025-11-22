d46cf265d1eb4c6a1276a172e1a64d9091cc8ed6
Loban Matin <loban.matin@gmail.com>
2025-11-22 13:24:12 +1030

    added spring boot project

---
+++ src/main/java/com/matin_devs/tally/TallyApplication.java
@@ -0,0 +1,13 @@
+package com.matin_devs.tally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/matin_devs/tally/model/User.java
@@ -0,0 +1,8 @@
+package com.matin_devs.tally.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class User {
+
+}
