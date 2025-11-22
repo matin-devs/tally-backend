9abe8eb752fd6ec676b474f43e68996dfa5c2e9d
Loban Matin <loban.matin@gmail.com>
2025-11-22 13:30:21 +1030

    create user entity

--- src/main/java/com/matin_devs/tally/model/User.java
+++ src/main/java/com/matin_devs/tally/model/User.java
@@ -1,8 +1,26 @@
 package com.matin_devs.tally.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@ToString
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Builder
+    public User(String username) {
+        this.username = username;
+    }
 }
